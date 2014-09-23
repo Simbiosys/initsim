9967fd4eaa803d874b1214e5d02253ffa565d248
javier-ruiz-trillo <javier.ruiz@simbiosys.es>
2014-09-23 13:31:40 +0200

    Cambios en grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,79 @@
 module.exports = function(grunt) {
 
   // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+grunt.initConfig({
+	pkg: grunt.file.readJSON('package.json'),
 	uglify: {
+		options: {
+			banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+		},
+		build: {
+			src: 'js/*.js',
+			dest: 'js/output.min.js'
+		}
+	},
+	sass: {
+		dist: {
+			options: {                       // Target options
+				style: 'compressed',
+				compass : true
+			},
+			files: [{
+				expand: true,
+				cwd: 'sass',
+				src: ['*.scss'],
+				dest: 'css',
+				ext: '.css'
+			}]
+		}
+	},
+	imagemin: {
 		dist: {
 			options: {
-				sourceMap: true,
-				banner: '/*! MyLib.js 1.0.0 | Aurelio De Rosa (@AurelioDeRosa) | MIT Licensed */'
+				optimizationLevel: 5
+			},
+			files: [{
+				expand: true,
+				cwd: 'img/',
+				src: ['**/*.{png,jpg,gif}'],
+				dest: 'dist/'
+			}]
+		}
+	},
+	cssmin:{
+		dist: {
+			options: {
+			banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
 			},
 			files: {
-				'dest/output.min.js': ['src/*.js'],
+				'css/screen.min.css': ['css/*.css']
+			}
+		}
+	},
+	jshint: {
+		// define the files to lint
+		files: ['gruntfile.js', 'js/*.js'],
+		// configure JSHint (documented at http://www.jshint.com/docs/)
+		options: {
+		// more options here if you want to override JSHint defaults
+			globals: {
+				jQuery: true,
+				console: true,
+				module: true
 			}
 		}
 	}
-  });
+});
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-sass');
+grunt.loadNpmTasks('grunt-contrib-imagemin');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-newer');
+  
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['jshint','newer:uglify','newer:sass','newer:imagemin','newer:cssmin']);
 
 };
